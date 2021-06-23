c2389d06408be56f311210cc6f07c6d7a44a9421
jpanchal <jiten.panchal@icx4..com>
2021-06-23 14:56:45 +0100

    jwt authentication service

---
+++ jwt-authentication-service/src/main/java/io/javabrains/jwtauthenticationservice/filters/AuthorizationFilter.java
@@ -0,0 +1,25 @@
+package io.javabrains.jwtauthenticationservice.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class AuthorizationFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		System.out.println("AuthorizationFilter executed");
+
+		filterChain.doFilter(request, response);
+	}
+
+}
---
+++ jwt-authentication-service/src/test/java/io/javabrains/jwtauthenticationservice/JwtAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.jwtauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
